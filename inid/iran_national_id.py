fcc2da1c411a8dd5712a0ccf894e283164e55ff3
RYN <ariyan.eghbal@gmail.com>
2020-02-27 22:29:34 +0330

    Create iterator for range generator

--- inid/iran_national_id.py
+++ inid/iran_national_id.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import sys
+import os
 import random
-
+import json
+from copy import deepcopy
+import pathlib
 
 def generate_id(prefix=''):
     if not isinstance(prefix, str):
@@ -32,6 +35,86 @@ def check_id(nid):
     r = nsum % 11
     checksum = r if r < 2 else 11 - r
     return checksum == int(nid[-1])
+
+code_file = os.path.join(pathlib.Path(__file__).parent.absolute(), 'city_codes.json')
+with open(code_file, 'r') as f:
+    CITY_CODES = json.load(f)
+    FLATTERN_CODES = []
+    for cities in CITY_CODES.values():
+        for codes in cities.values():
+            FLATTERN_CODES.extend(codes)
+    
+
+class INID:
+    def __ini__(self, inid):
+        if not isinstance(inid, str) or len(inid) not in (9,10):
+            raise ValueError("Bad INID ({inid})")
+        self._value = inid
+
+    @property
+    def value(self):
+        if len(self._value) == 9:
+            return generate_id(self._value)
+        return self._value
+
+    def __str__(self):
+        return self.value
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    def is_valid(self):
+        return check_id(self.vlue)
+
+
+class INID_RANGE:
+    def __init__(self, start='000000', end='999999', city_codes=None):
+        self._start = start
+        self._end = end
+        self._city_codes = city_codes
+        self._current = int(self._start)
+        self._stop = int(self._end)
+        self._current_city = None 
+        if self._city_codes:
+            self._cities = deepcopy(self._city_codes)
+        else:
+            self._cities = deepcopy(FLATTERN_CODES)
+
+
+    def __iter__(self):
+        self._current = int(self._start)
+        if self._city_codes:
+            self._cities = deepcopy(self._city_codes)
+        else:
+            self._cities = deepcopy(FLATTERN_CODES)
+        return self
+ 
+    def __next__(self):
+
+        if not self._current_city:
+            try:
+                self._current_city = self._cities.pop(0)
+            except IndexError as e:
+                raise StopIteration()
+
+        if self._current <= self._stop:
+            c = self._current
+            self._current += 1
+            return INID(f"{self._current_city}{c:06d}")
+        else:
+            self._current_city = None
+            self._start = int(self._start) 
+            
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
